3b499046f512e430e818b26d3accf0ad48f16ac5
Luke Safly <Lukesafly01@gmail.com>
2020-08-10 16:18:35 -0400

    Finished Homework

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace OurDotNetApp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      Console.WriteLine("Welcome to C#. My name is Luke. I am a student at SDG.");
     }
   }
 }
